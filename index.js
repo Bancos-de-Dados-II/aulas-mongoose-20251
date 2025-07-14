42cfeb915e4a95a7ce3f9c1008f6b3b394069fa5
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 16:34:02 -0300

    Definindo o modelo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 //Conectando ao MongoDB
 main();
@@ -7,3 +7,17 @@ async function main() {
         'mongodb://localhost:27017/aula');
     console.log('Conectado ao MongoDB');
 }
+
+//Definindo o schema
+const produtoSchema = new mongoose.Schema({
+    descricao: String,
+    preco: Number,
+    categoria:{
+        type: String,
+        enum: ['Alimentício', 'Limpeza', 'Higiene']
+    },
+    validade: {
+        type: Date,
+        default: Date.now
+    }
+});
